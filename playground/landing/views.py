98a5e150894d2fe57e03be681cbc3b5d163785f4
Armando Urquiola <patchamama.go@gmail.com>
2025-09-10 18:54:50 +0200

    66 For tag

--- playground/landing/views.py
+++ playground/landing/views.py
@@ -4,8 +4,11 @@ from datetime import date
 
 # Create your views here.
 def home(request):
+    stack = ['Python', 'Django', 'JavaScript', 'React']
+    
     return render(request, 'landing/landing.html', {
         "name": "Mandy",
         "age": 30,
-        "current_date": date.today()
+        "current_date": date.today(),
+        "stack": stack
     })
