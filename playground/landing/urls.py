2c5fb057c898381406b8c3a256c409941712ff34
Armando Urquiola <patchamama.go@gmail.com>
2025-09-10 19:06:16 +0200

    67 URLs dinámicas y URLs tag

--- playground/landing/urls.py
+++ playground/landing/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'), # http://127.0.0.1:8000/home
+    path('stack/<str:tool>/', views.stack_detail, name='stack'), # http://127.0.0.1:8000/stack/Python/
 ]
--- playground/landing/views.py
+++ playground/landing/views.py
@@ -1,14 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import date
 
 
 # Create your views here.
 def home(request):
-    stack = ['Python', 'Django', 'JavaScript', 'React']
-    
+    # stack = ['Python', 'Django', 'JavaScript', 'React']
+    stack = [
+        {'id': "python", 'name': 'Python', 'proficiency': 'Advanced'},
+        {'id': "django", 'name': 'Django', 'proficiency': 'Intermediate'},
+        {'id': "javascript", 'name': 'JavaScript', 'proficiency': 'Intermediate'},]
+
     return render(request, 'landing/landing.html', {
         "name": "Mandy",
         "age": 30,
         "current_date": date.today(),
         "stack": stack
     })
+    
+def stack_detail(request, tool):
+    return HttpResponse(f"Detail of tool: {tool}")
+    # return render(request, 'landing/stack_detail.html', {
+    #     "tool": tool
+    # })
